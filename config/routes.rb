bd02c6ce27be229f2cf627d42a3db051ae704018
philmill <phil@philmill.net>
2019-05-15 21:24:12 -0400

    fix: use restful route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'login', to: 'sessions/create'
+      post 'login', to: 'sessions#create'
       resources :ideas, only: [:index, :create, :update, :destroy]
     end
   end
